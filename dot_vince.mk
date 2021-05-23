4d8aea519a7159824e3b4ccbb4d06c00c87906d6
aseelps <aseelpsathar@gmail.com>
2021-05-23 19:05:01 +0530

    vince: added Faceulock support
    Signed-off-by: aseelps <aseelpsathar@gmail.com>

--- dot_vince.mk
+++ dot_vince.mk
@@ -31,6 +31,7 @@ $(call inherit-product, device/xiaomi/vince/device.mk)
 $(call inherit-product, vendor/dot/config/common.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
+TARGET_FACE_UNLOCK_SUPPORTED := true
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := vince
